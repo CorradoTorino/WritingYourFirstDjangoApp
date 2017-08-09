cb37db09e353dd7d930912e6f2265f69e9d1581d
Corrado C <corrado.torino@gmail.com>
2017-08-09 15:38:49 +0200

    Fixing a conceptual error. The test are running in a special db instance. it is not necessary to add entries in the production db.

--- polls/tests.py
+++ polls/tests.py
@@ -1,17 +1,14 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from unittest import skip
 import datetime
 
 from .models import Question
 
 class QuestionModeltests(TestCase):
-    @skip("This test can pass only in the first 30 days from the creation. It depends on the entry in the db.")
     def test_was_published_recently_with_future_question_return_false(self):
         """
         was_published_recently() return false when is the question is pubblished in the future.
-        However this test is a dummy test. it depends on the state of the db. see skip note.
         """
         
         time = timezone.now() + datetime.timedelta(days=30)
