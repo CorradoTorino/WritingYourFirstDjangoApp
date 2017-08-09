634134bd7b960617be7b7c79e85218f3aa3bc446
Corrado C <corrado.torino@gmail.com>
2017-08-09 10:07:47 +0200

    Finished Part 4. Made use of generic view provided by django in order to reduce code for polls/views

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,28 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list,}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+    def get_last_five_pubblished_questions(self):
+        return Question.objects.order_by('-pub_date')[:5]
+    
+    def get_queryset(self):
+        return self.get_last_five_pubblished_questions() 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/results.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+        
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
