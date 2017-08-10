0fe84563c1fac06f61058622aff576af3218ed6a
Corrado C <corrado.torino@gmail.com>
2017-08-10 10:15:13 +0200

    Part 7, Customized the Question admin view using fieldsets to split the fields

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Choice, Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Question',                   {'fields': ['question_text']}),
+        ('DateTime Information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
