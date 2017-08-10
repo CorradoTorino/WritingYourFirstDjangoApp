8bf0a7fd5ffedf68af0bee237172aa6d893e49a6
Corrado C <corrado.torino@gmail.com>
2017-08-10 10:59:06 +0200

    Part 7. Customized the Question admin view to add related choices in the same view using a StackedInline

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import Choice, Question
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Question',                   {'fields': ['question_text']}),
         ('DateTime Information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
