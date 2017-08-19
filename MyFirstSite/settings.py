1bce134db2ae939c6aebcb3232e0ab2cdb467ddb
Corrado C <corrado.torino@gmail.com>
2017-08-20 01:16:54 +0200

    PostgreSQL migration: Changed db in settings.py and deleted sqlite db

--- MyFirstSite/settings.py
+++ MyFirstSite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MyFirstSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polls',
+        'USER': 'pollsuser',
+        'PASSWORD': 'pollsOnPostgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
